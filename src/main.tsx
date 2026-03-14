a341cad287711c651d6f269988ca0f3f4848a92c
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-03-14 23:54:12 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FootingBBSCalculator from './FootingBBS'
+import FootingBBSCalculator from './SlabBBS'
 import './style.css'
 
 const rootElement = document.getElementById('root') || document.getElementById('app');
